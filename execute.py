c69f01525db07508670bcafe426651eddcaaba78
monkut <5i!v3r-gh>
2017-10-20 14:04:08 +0900

    Update to remove depreciation warning

--- execute.py
+++ execute.py
@@ -117,7 +117,7 @@ def create_model(session, forward_only):
     model.saver.restore(session, ckpt.model_checkpoint_path)
   else:
     print("Created model with fresh parameters.")
-    session.run(tf.initialize_all_variables())
+    session.run(tf.global_variables_initializer())
   return model
 
 
